3d2b82aae273942c9a85fcd83487a9d2e7f63152
Renat Zhilkibaev <rzhilkibaev@hotwire.com>
2014-08-22 17:44:23 -0700

    Update dependencies and maven plutings Add simple integration test

--- demo-store/src/main/java/org/simplemart/product/ProductApp.java
+++ demo-store/src/main/java/org/simplemart/product/ProductApp.java
@@ -1,12 +1,18 @@
 package org.simplemart.product;
 
 import io.dropwizard.Application;
+import io.dropwizard.lifecycle.ServerLifecycleListener;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
 import org.simplemart.product.resources.ProductResource;
 
-public class ProductApp extends Application<ProductConfiguration> {
+public class ProductApp extends Application<ProductConfiguration> implements ServerLifecycleListener {
+
+    private volatile String host;
+    private volatile int localPort;
 
     public static void main(String[] args) throws Exception {
         new ProductApp().run(args);
@@ -15,12 +21,26 @@ public class ProductApp extends Application<ProductConfiguration> {
     @Override
     public void initialize(Bootstrap<ProductConfiguration> bootstrap) {
         // TODO Auto-generated method stub
+    }
 
+    @Override
+    public void serverStarted(Server server) {
+        host = ((ServerConnector) server.getConnectors()[0]).getHost();
+        localPort = ((ServerConnector) server.getConnectors()[0]).getLocalPort();
     }
 
     @Override
     public void run(ProductConfiguration configuration, Environment environment) throws Exception {
+        environment.lifecycle().addServerLifecycleListener(this);
         environment.jersey().register(new ProductResource());
+    }
+
+    String getHost() {
+        return host;
+    }
+
+    int getLocalPort() {
+        return localPort;
     }
 
 }
--- demo-store/src/test/java/org/simplemart/product/ProductSmokeIT.java
+++ demo-store/src/test/java/org/simplemart/product/ProductSmokeIT.java
@@ -6,20 +6,28 @@ import org.junit.Test;
 import org.simplemart.product.resources.Product;
 
 import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.WebResource;
 
 public class ProductSmokeIT {
 
-    private final WebResource productResource = new Client().resource("http://localhost:8080/product/abc");
+    private static volatile String host = "localhost";
+    private static volatile int port;
 
     @BeforeClass
     public static void before() throws Exception {
-        new ProductApp().run(new String[] {"server", "target/classes/product.yml"});
+        ProductApp app = new ProductApp();
+        app.run(new String[] {"server", "target/test-classes/product-IT.yml" });
+        host = nvl(app.getHost(), host);
+        port = app.getLocalPort();
     }
 
     @Test
     public void product_is_returned_when_request_is_made() {
-        Product p = productResource.get(Product.class);
+        Product p = new Client().resource("http://" + host + ":" + port + "/product/abc").get(Product.class);
         Assertions.assertThat(p.getCaption()).as("product caption").isEqualTo("abc");
     }
+
+    private static <T> T nvl(T test, T def) {
+        return test == null ? def : test;
+    }
+
 }
